cabef44a7ee31b198d2d24baa6b3ddcd115c6c9b
dashlexz1 <adr1999@icloud.com>
2024-04-04 22:12:33 +0300

    DIcommit

--- src/main/java/koschei/KoscheiTheDeathless.java
+++ src/main/java/koschei/KoscheiTheDeathless.java
@@ -1,20 +1,27 @@
 package koschei;
 
-import koschei.models.Ocean1;
+import koschei.models.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
 @Component
 public class KoscheiTheDeathless {
 
     private Ocean1 ocean;
+    private Duck5 duck5;
 
     public String getRulesByDeth() {
-        return "На свете есть океан , " + ocean.toString();
+        return "На свете есть океан , " + ocean.toString() + duck5.toString();
     }
 
     @Autowired
     public void setOcean(Ocean1 ocean) {
         this.ocean = ocean;
     }
+
+    @Autowired
+    public void setDuck5(Duck5 duck5) {
+        this.duck5 = duck5;
+    }
 }
+
+
--- src/main/java/koschei/models/Ocean1.java
+++ src/main/java/koschei/models/Ocean1.java
@@ -2,15 +2,18 @@ package koschei.models;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
 @Component
 public class Ocean1 {
 
-    @Autowired
     private Island2 island;
+    @Autowired
+    public Ocean1(Island2 island) {
+        this.island = island;
+    }
 
     @Override
     public String toString() {
         return "на океане остров " + island.toString();
     }
+
 }
